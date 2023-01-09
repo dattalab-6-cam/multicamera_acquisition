47f20baf051e32241331d435ff71db7ee55ce145
Tim Sainburg <timsainb@gmail.com>
2023-01-09 18:24:49 -0500

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='multicamera_acquisition',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python packaamera acquisition with Flir and Basler cameras.',
+    author='dattalab',
+    license='MIT',
+)
