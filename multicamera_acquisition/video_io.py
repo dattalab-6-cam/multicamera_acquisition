aaee71ce95be55a30c15080b979a003a923460f6
Jack Lovell <jack_lovell@hms.harvard.edu>
2023-07-28 10:33:57 -0400

    fixed depth encoding issue

--- multicamera_acquisition/video_io.py
+++ multicamera_acquisition/video_io.py
@@ -19,7 +19,7 @@ def count_frames(file_name):
 
 
 bytes_to_pad = np.repeat(np.uint8(128), int((1200 * 1920)/2)).tobytes()
-def write_frame(filename, frame, fps, quality=15, pixel_format="grey8", gpu=None, pipe=None):
+def write_frame(filename, frame, fps, quality=15, pixel_format="grey8", gpu=None, pipe=None, depth=False):
     """
     Write frames to a video file.
 
@@ -45,48 +45,90 @@ def write_frame(filename, frame, fps, quality=15, pixel_format="grey8", gpu=None
         # Get the size of the frame
         frame_size = "{0:d}x{1:d}".format(frame.shape[1], frame.shape[0])
         logging.log(logging.INFO, f"FRAME SHAPE {frame.shape}")
-        # Prepare the basic ffmpeg command
-        command = [
-            "ffmpeg",
-            "-y",  # Overwrite existing file without asking
-            "-f", "rawvideo",
-            "-vcodec", "rawvideo",
-            #"-hwaccel",
-            #"cuda",
-            "-pix_fmt", pixel_format,  # Input pixel format (gray8, gray16, etc.)
-            "-s", frame_size,  # Input frame size
-            "-r", str(fps),  # Input frames per second
-            "-i", "-",  # Read input from stdin
-            "-an",  # No audio
+        if not depth:
+            # Prepare the basic ffmpeg command
+            command = [
+                "ffmpeg",
+                "-y",  # Overwrite existing file without asking
+                "-f", "rawvideo",
+                "-vcodec", "rawvideo",
+                "-pix_fmt", pixel_format,  # Input pixel format (gray8, gray16, etc.)
+                "-s", frame_size,  # Input frame size
+                "-r", str(fps),  # Input frames per second
+                "-i", "-",  # Read input from stdin
+                "-an",  # No audio
 
-        ]
+            ]
 
-        if gpu is not None:
-            # GPU encoding options using h264_nvenc codec
+            if gpu is not None:
+                # GPU encoding options using h264_nvenc codec
+                command += [
+                    "-c:v", "h264_nvenc",
+                    "-preset", "p1",
+                    "-qp", str(quality),  # Video quality (0-51, lower is better)
+                    "-gpu", str(gpu),  # Specify which GPU to use for encoding
+                    "-vsync", "0",  # Disable frame rate synchronization
+                    "-2pass", "0",  # Disable two-pass encoding
+                ]
+            else:
+                # CPU encoding options using libx264 codec
+                if pixel_format in ["gray16", "grey16"]:
+                    command+= ["-vcodec", "ffv1"]
+                else:
+                    command+= ["-c:v", "libx264"]
+                command += [
+                    "-preset", "ultrafast",
+                    "-crf", str(quality),  # Video quality (0-51, lower is better)
+                    "-threads", "4",  # Number of threads to use for encoding
+                ]
+
+            if pixel_format not in ["gray16", "grey16"]:
+                command+= ["-pix_fmt", "yuv420p"] # Output pixel format
+
+            # Additional options for output format and filename
             command += [
-                "-c:v", "h264_nvenc",
-                "-preset", "p1",
-                "-qp", str(quality),  # Video quality (0-51, lower is better)
-                "-gpu", str(gpu),  # Specify which GPU to use for encoding
-                "-vsync", "0",  # Disable frame rate synchronization
-                "-2pass", "0",  # Disable two-pass encoding
+                str(filename)  # Output filename
             ]
         else:
-            # CPU encoding options using libx264 codec
-            command += [
-                "-c:v", "libx264",
-                "-preset", "ultrafast",
-                "-crf", str(quality),  # Video quality (0-51, lower is better)
+            crf = 14
+            codec = "ffv1"
+            threads = 6
+            slices = 24
+            slicecrc = 1
+            command = [
+                "ffmpeg",
+                "-y",
+                #"-loglevel",
+                #"fatal",
+                "-framerate",
+                str(fps),
+                "-f",
+                "rawvideo",
+                "-s",
+                frame_size,
+                "-pix_fmt",
+                pixel_format,
+                "-i",
+                "-",
+                "-an",
+                #"-crf",
+                #str(crf),
+                "-vcodec",
+                codec,
+                #"-preset",
+                #"ultrafast",
+                #"-threads",
+                #str(threads),
+                #"-slices",
+                #str(slices),
+                #"-slicecrc",
+                #str(slicecrc),
+                #"-r",
+                #str(fps),
+                str(filename),
             ]
 
-        # Additional options for output format and filename
-        command += [
-            #"-threads", "4",  # Number of threads to use for encoding
-            "-pix_fmt", "yuv420p",#"yuv420p",  # Output pixel format
-            str(filename)  # Output filename
-        ]
-
-        # print(' '.join(command))  # Print the ffmpeg command for debugging purposes
+        print(' '.join(command))  # Print the ffmpeg command for debugging purposes
 
         # Create a subprocess pipe to write frames
         pipe = subprocess.Popen(command, stdin=subprocess.PIPE, stderr=subprocess.DEVNULL)
