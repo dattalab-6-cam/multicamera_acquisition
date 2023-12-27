902ea324c5faf9c3038543152cc175495a302857
JP_6cam_rig <jonahpearl@g.harvard.edu>
2023-12-26 21:39:56 -0500

    add viz func from notebook

--- multicamera_acquisition/visualization.py
+++ multicamera_acquisition/visualization.py
@@ -7,7 +7,8 @@ import cv2
 import numpy as np
 import logging
 import time
-
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def get_latest(queue, timeout=0.1):
     start_time = time.time()
@@ -162,3 +163,55 @@ def normalize_array(frame, min_value=None, max_value=None):
         (frame - min_value) / (max_value - min_value)
     ) * 255.0  # Normalize to [0, 255]
     return frame.astype(np.uint8)
+
+
+def plot_video_stats(csv_path, name):
+
+    # Load the data
+    df = pd.read_csv(csv_path)
+
+    # Set up plot
+    fig, axs = plt.subplots(
+        ncols = 1, 
+        nrows = 5, 
+        gridspec_kw= {'height_ratios':[1,1,1,1,1]},
+        figsize=(10,8),
+        sharex=True
+    )
+    
+    # Plot frame diffs (ie, check for dropped frames)
+    axs[0].set_title(f"{name}: frame diff (dropped frames?)")
+    axs[0].plot(np.diff(df.frame_id.values))
+
+    # Plot camera timestamp diffs
+    diffs = np.diff(df.frame_timestamp.values)
+    axs[1].set_title(f"{name}: camera timestamp diff")
+    axs[1].plot(diffs / np.median(diffs))
+
+    # Plot computer timestamp diffs
+    axs[2].set_title(f"{name}: computer timestamp (uid) diff")
+    axs[2].plot(np.diff(df.frame_image_uid.values) / np.median(np.diff(df.frame_image_uid.values)))
+
+    # Plot queue size
+    axs[3].set_title(f"{name}: queue size")
+    axs[3].plot(df.queue_size.values)
+    axs[3].set_xlabel('Frames')
+    axs[3].set_title('Queue size')
+
+    # Plot relative occurrence of framerates
+    axs[4].hist(1/(np.diff(df.frame_timestamp.values)* 1e-9), bins=100);
+    axs[4].set_xlabel('Framerate')
+    axs[4].set_ylabel('Count')
+    axs[4].set_title(f"{name}: framerate histogram")
+    
+    # Format plot
+    plt.tight_layout()
+    plt.show()
+    
+    # Print some info 
+    time_elapsed = (df.frame_timestamp.values[-1] - df.frame_timestamp.values[0]) * 1e-9
+    avg_diffs = np.mean(diffs)
+    print(f"Total time elapsed: {time_elapsed} seconds")
+    print(f"Average framerate: {1 / (avg_diffs* 1e-9)} Hz")
+
+    return
