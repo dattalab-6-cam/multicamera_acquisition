563fc62cc47c8d3e78746c37c89020f946f532ef
Kai Fox <kai_fox@hms.harvard.edu>
2024-01-03 15:27:26 -0500

    rough refactor

--- multicamera_acquisition/tests/visualization/test_visualization.py
+++ multicamera_acquisition/tests/visualization/test_visualization.py
@@ -9,11 +9,11 @@ import os
 import pytest
 
 
-from multicamera_acquisition.writer import FFMPEG_Writer
+from multicamera_acquisition.visualization import refactor_MultiDisplay
 
 from multicamera_acquisition.video_io_ffmpeg import count_frames
 
-from multicamera_acquisition.writer import (
+from multicamera_acquisition.tests.writer.test_writers import (
     get_DummyFrames_process,
     fps,
     n_test_frames,
@@ -21,47 +21,45 @@ from multicamera_acquisition.writer import (
 )
 
 
-
-
 @pytest.fixture(scope="function")
-def nvc_writer_processes(tmp_path, fps, n_test_frames):
+def multidisplay_processes(tmp_path, fps, n_test_frames):
     """Generate linked NVC_Writer and DummyFrames processes for testing
     """
-    from multicamera_acquisition.writer import NVC_Writer
-    config = NVC_Writer.default_writer_config(fps)
-    config["camera_name"] = "test"
-    queue = mp.Queue()
-    dummy_frames_proc = get_DummyFrames_process(fps, queue, n_test_frames)
-    writer = NVC_Writer(
-        queue, 
-        video_file_name=tmp_path / "test.mp4",
-        metadata_file_name=tmp_path / "test.csv",
+    cameras = ['top', 'bottom']
+    config = refactor_MultiDisplay.default_display_config(cameras)
+    queues = [mp.Queue() for c in cameras]
+    dummy_frames_procs = [
+        get_DummyFrames_process(fps, queue, n_test_frames)
+        for queue in queues
+    ]
+    display = refactor_MultiDisplay(
+    # display = None
+        queues,
         config=config,
     )
-    return (writer, dummy_frames_proc)
+    return (display, dummy_frames_procs)
 
-
-def test_NVC_writer(nvc_writer_processes, n_test_frames):
+def test_MultiDisplay(multidisplay_processes, n_test_frames):
 
     # Get the writer and dummy frames proc
-    writer, dummy_frames_proc = nvc_writer_processes
-
-    # Check that the writer is not running
-    assert writer.frame_id == 0
-
+    display, dummy_frames_procs = multidisplay_processes
+    
     # Start the writer and dummy frames proc
-    writer.start()
-    dummy_frames_proc.start()
+    # display.start()
+    for proc in dummy_frames_procs:
+        proc.start()
 
     # Wait for the processes to finish
-    dummy_frames_proc.join(timeout=60)
-    writer.join(timeout=60)
+    for proc in dummy_frames_procs:
+        proc.join(timeout=60)
+
+    display.join(timeout=60)
 
     # NB: this won't work! Because multiprocessing stuff runs in its own scope.
     # Would need to use a pipe / shared value to communicate between the two.
     # assert writer.frame_id == 20
 
     # Check that the video exists
-    assert writer.video_file_name.exists()
-    assert count_frames(str(writer.video_file_name).replace(".mp4", ".muxed.mp4")) == n_test_frames
+    # assert writer.video_file_name.exists()
+    # assert count_frames(str(writer.video_file_name).replace(".mp4", ".muxed.mp4")) == n_test_frames
 
--- multicamera_acquisition/visualization.py
+++ multicamera_acquisition/visualization.py
@@ -9,6 +9,159 @@ import logging
 import time
 import matplotlib.pyplot as plt
 import pandas as pd
+from collections.abc import Iterable
+from numbers import Number
+
+
+class refactor_MultiDisplay(mp.Process):
+
+    def __init__(self, queues, config = None):
+        super().__init__()
+
+        # Store params
+        self.config = config
+
+        self.pipe = None
+        self.queues = queues
+        self.camera_names = config['camera_names']
+        self.num_cameras = len(self.camera_names)
+        self.downsample = config['downsample']
+        self.cameras_per_row = config['cameras_per_row']
+        self.display_size = config['size']
+        self.display_ranges = config['ranges']
+
+        # Set up the config
+        if config is None:
+            self.config = self.default_display_config()
+        else:
+            self.validate_config()
+
+
+    def _init_layout(self):
+
+        root = tk.Tk()
+        xdim = self.display_size[0] * self.cameras_per_row
+        ydim = self.display_size[1] * int(
+            np.ceil(self.num_cameras / self.cameras_per_row)
+        )
+        root.title("Camera view")  # this is the title of the window
+        root.geometry(f"{xdim}x{ydim}")  # this is the size of the window
+
+        rowi = 0
+        labels = []
+        # create a label to hold the image
+        for ci, camera_name in enumerate(self.camera_names):
+            # create the camera name label
+            label_text = tk.Label(root, text=camera_name)
+            label_text.grid(row=rowi, column=ci % self.cameras_per_row, sticky="nsew")
+
+            # create the camerea image label
+            label = tk.Label(root)  # this is where the image will go
+            label.grid(row=rowi + 1, column=ci % self.cameras_per_row, sticky="nsew")
+
+            if (ci + 1) % self.cameras_per_row == 0:
+                rowi += 2
+
+            labels.append(label)
+
+        for i in range(self.cameras_per_row):
+            root.grid_columnconfigure(i, weight=1)
+        for i in range(rowi):
+            root.grid_rowconfigure(i, weight=1)
+
+        return root, labels
+
+    
+    def _fetch_images(self, queue, camera_name, log_if_error):
+        try:
+            if queue.qsize() > 1:
+                while queue.qsize() > 1:
+                    data = get_latest(queue, timeout=0.01)
+            else:
+                data = queue.get(timeout=0.01)
+        except Exception as error:
+            if log_if_error:
+                logging.info(
+                    "{}: Timeout occurred {}".format(
+                        camera_name, str(error)
+                    )
+                )
+            return [None]
+        return data
+    
+
+    def run(self):
+        
+        root, labels = self._init_layout()
+
+        quit = False
+        while True:
+            # initialized checks to see if recording has started
+            initialized = np.zeros(len(self.queues)).astype(bool)
+            for qi, (queue, camera_name) in enumerate(zip(self.queues, self.camera_names)):
+                
+                data = self._fetch_images(
+                    queue,
+                    camera_name,
+                    log_if_error = initialized[qi])
+
+                if len(data) == 0:
+                    quit = True
+                    break
+
+                # retrieve frame
+                if data[0] is not None:
+                    initialized[qi] = True
+                    img = format_frame(
+                        data[0],
+                        downsample = self.downsample,
+                        display_size = self.display_size,
+                        display_range = self.display_ranges[qi],
+                        is_depth = data[0].dtype == np.uint16 or ("lucid" in camera_name))
+                    
+                    # update label with new image
+                    labels[qi].config(image=img)
+                    labels[qi].image = img
+                else:
+                    continue
+                    # print(f"No data: {self.camera_names[qi]}")
+
+            if quit:
+                break
+            # update tkinter window
+            root.update()
+        root.destroy()
+
+
+    @staticmethod
+    def default_display_config(camera_names, ):
+        return {
+            'camera_names': camera_names,
+            'ranges': [(0, 256) for _ in camera_names],
+            'downsample': 4,
+            'cameras_per_row': 3,
+            'size': (300, 300),
+        }
+    
+
+    def validate_config(self):
+        
+        pass
+        # for k in refactor_MultiDisplay.default_display_config([]):
+        #     assert 'camera_names' in self.config, f"Display config missing '{k}'"
+        
+        # assert (
+        #     isinstance(self.config['camera_names'], Iterable),
+        # ), "Display config `camera_names` must be a list or tuple."
+        # assert (
+        #     isinstance(self.config['ranges'], Iterable) and
+        #     isinstance(self.config['ranges'][0], Iterable) and
+        #     len(self.config['ranges'][0]) == 2
+        # ), ""
+
+
+
+        
 
 def get_latest(queue, timeout=0.1):
     start_time = time.time()
@@ -31,25 +184,94 @@ def get_latest(queue, timeout=0.1):
 
     return item
 
-class refactor_MultiDisplay(mp.Process):
 
-    def __init__(self, queue, config = None):
-        super().__init__()
+def format_frame(frame, downsample, display_size, display_range, is_depth):
 
-        # Store params
-        self.queue = queue
-        self.config = config
+    frame = frame[:: downsample, :: downsample]
+    frame = cv2.resize(frame, display_size)
 
-        # Set up the config
-        if config is None:
-            self.config = self.default_writer_config()
+    # int16 should be azure data
+    if is_depth:
+        # normalize in range
+        if display_range is not None:
+            frame = normalize_array(
+                frame,
+                min_value=display_range[0],
+                max_value=display_range[1],
+            ).astype(np.uint8)
         else:
-            self.validate_config()
+            frame = normalize_array(frame).astype(np.uint8)
+
+        # Convert frame to turbo/jet colormap
+        frame = cv2.applyColorMap(frame, cv2.COLORMAP_TURBO)
+
+    return PIL.Image.fromarray(frame)
 
 
-    def run(self):
 
-        
+def normalize_array(frame, min_value=None, max_value=None):
+    if min_value is None:
+        min_value = np.min(frame)
+        max_value = np.max(frame)
+    frame[frame > max_value] = min_value
+    frame[frame < min_value] = min_value
+    # frame = np.clip(frame, min_value, max_value)  # Ensure values are in the range [min_value, max_value]
+    frame = (
+        (frame - min_value) / (max_value - min_value)
+    ) * 255.0  # Normalize to [0, 255]
+    return frame.astype(np.uint8)
+
+
+def plot_video_stats(csv_path, name):
+
+    # Load the data
+    df = pd.read_csv(csv_path)
+
+    # Set up plot
+    fig, axs = plt.subplots(
+        ncols = 1, 
+        nrows = 5, 
+        gridspec_kw= {'height_ratios':[1,1,1,1,1]},
+        figsize=(10,8),
+        sharex=True
+    )
+    
+    # Plot frame diffs (ie, check for dropped frames)
+    axs[0].set_title(f"{name}: frame diff (dropped frames?)")
+    axs[0].plot(np.diff(df.frame_id.values))
+
+    # Plot camera timestamp diffs
+    diffs = np.diff(df.frame_timestamp.values)
+    axs[1].set_title(f"{name}: camera timestamp diff")
+    axs[1].plot(diffs / np.median(diffs))
+
+    # Plot computer timestamp diffs
+    axs[2].set_title(f"{name}: computer timestamp (uid) diff")
+    axs[2].plot(np.diff(df.frame_image_uid.values) / np.median(np.diff(df.frame_image_uid.values)))
+
+    # Plot queue size
+    axs[3].set_title(f"{name}: queue size")
+    axs[3].plot(df.queue_size.values)
+    axs[3].set_xlabel('Frames')
+    axs[3].set_title('Queue size')
+
+    # Plot relative occurrence of framerates
+    axs[4].hist(1/(np.diff(df.frame_timestamp.values)* 1e-9), bins=100);
+    axs[4].set_xlabel('Framerate')
+    axs[4].set_ylabel('Count')
+    axs[4].set_title(f"{name}: framerate histogram")
+    
+    # Format plot
+    plt.tight_layout()
+    plt.show()
+    
+    # Print some info 
+    time_elapsed = (df.frame_timestamp.values[-1] - df.frame_timestamp.values[0]) * 1e-9
+    avg_diffs = np.mean(diffs)
+    print(f"Total time elapsed: {time_elapsed} seconds")
+    print(f"Average framerate: {1 / (avg_diffs* 1e-9)} Hz")
+
+    return
 
 
 
@@ -171,68 +393,3 @@ class MultiDisplay(mp.Process):
             # update tkinter window
             root.update()
         root.destroy()
-
-
-def normalize_array(frame, min_value=None, max_value=None):
-    if min_value is None:
-        min_value = np.min(frame)
-        max_value = np.max(frame)
-    frame[frame > max_value] = min_value
-    frame[frame < min_value] = min_value
-    # frame = np.clip(frame, min_value, max_value)  # Ensure values are in the range [min_value, max_value]
-    frame = (
-        (frame - min_value) / (max_value - min_value)
-    ) * 255.0  # Normalize to [0, 255]
-    return frame.astype(np.uint8)
-
-
-def plot_video_stats(csv_path, name):
-
-    # Load the data
-    df = pd.read_csv(csv_path)
-
-    # Set up plot
-    fig, axs = plt.subplots(
-        ncols = 1, 
-        nrows = 5, 
-        gridspec_kw= {'height_ratios':[1,1,1,1,1]},
-        figsize=(10,8),
-        sharex=True
-    )
-    
-    # Plot frame diffs (ie, check for dropped frames)
-    axs[0].set_title(f"{name}: frame diff (dropped frames?)")
-    axs[0].plot(np.diff(df.frame_id.values))
-
-    # Plot camera timestamp diffs
-    diffs = np.diff(df.frame_timestamp.values)
-    axs[1].set_title(f"{name}: camera timestamp diff")
-    axs[1].plot(diffs / np.median(diffs))
-
-    # Plot computer timestamp diffs
-    axs[2].set_title(f"{name}: computer timestamp (uid) diff")
-    axs[2].plot(np.diff(df.frame_image_uid.values) / np.median(np.diff(df.frame_image_uid.values)))
-
-    # Plot queue size
-    axs[3].set_title(f"{name}: queue size")
-    axs[3].plot(df.queue_size.values)
-    axs[3].set_xlabel('Frames')
-    axs[3].set_title('Queue size')
-
-    # Plot relative occurrence of framerates
-    axs[4].hist(1/(np.diff(df.frame_timestamp.values)* 1e-9), bins=100);
-    axs[4].set_xlabel('Framerate')
-    axs[4].set_ylabel('Count')
-    axs[4].set_title(f"{name}: framerate histogram")
-    
-    # Format plot
-    plt.tight_layout()
-    plt.show()
-    
-    # Print some info 
-    time_elapsed = (df.frame_timestamp.values[-1] - df.frame_timestamp.values[0]) * 1e-9
-    avg_diffs = np.mean(diffs)
-    print(f"Total time elapsed: {time_elapsed} seconds")
-    print(f"Average framerate: {1 / (avg_diffs* 1e-9)} Hz")
-
-    return
