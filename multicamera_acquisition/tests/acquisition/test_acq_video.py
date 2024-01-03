168d23065f5002c03cc7c20f5ee78ea4467e655e
jonahpearl <j4jonahp@gmail.com>
2024-01-03 16:47:46 -0500

    mux test (untested)

--- multicamera_acquisition/tests/acquisition/test_acq_video.py
+++ multicamera_acquisition/tests/acquisition/test_acq_video.py
@@ -17,6 +17,7 @@ from multicamera_acquisition.video_io_ffmpeg import (
 
 from multicamera_acquisition.writer import (
     FFMPEG_Writer,
+    NVC_Writer,
 )
 
 from multicamera_acquisition.tests.writer.test_writers import (
@@ -69,5 +70,49 @@ def test_refactor_acquire_video(tmp_path, camera_brand, n_test_frames, fps):
         assert os.path.exists(save_loc / f"{camera_name}.metadata.csv")
 
     # Check that the video has the right number of frames
+    for camera_name in full_config["cameras"].keys():
+        assert count_frames(str(save_loc / f"{camera_name}.mp4")) == n_test_frames
+
+
+def test_refactor_acquire_video_muxing(tmp_path, camera_brand, n_test_frames, fps):
+    camera_list = [
+        {"name": "top", "brand": camera_brand, "id": 0, "short_name": "continuous"},
+        {"name": "bottom", "brand": camera_brand, "id": 1, "short_name": "continuous"}
+    ]
+
+    # Parse the "camera list" into a partial config
+    partial_new_config = partial_config_from_camera_list(camera_list, fps)
+
+    # Add NVC writers to each camera
+    nvc_writer_config = NVC_Writer.default_writer_config(fps)
+    nvc_writer_config["auto_remux_videos"] = True  # this is the default, but just to make it explicit / in case we change the default
+    for camera_name in partial_new_config["cameras"].keys():
+        nvc_writer_config["camera_name"] = camera_name
+        partial_new_config["cameras"][camera_name]["writer"] = nvc_writer_config
+
+    # Create the full config, filling in defaults where necessary
+    full_config = create_full_camera_default_config(partial_new_config)
+
+    # Set up the acquisition loop part of the config
+    acq_config = AcquisitionLoop.default_acq_loop_config()
+    acq_config["max_frames_to_acqure"] = n_test_frames
+    full_config["acq_loop"] = acq_config
+
+    # Run the func!
+    save_loc, full_config = refactor_acquire_video(
+        tmp_path,
+        full_config,
+        recording_duration_s=5,
+        append_datetime=True,
+        overwrite=False,
+    )
+
+    # Check that the video exists
+    for camera_name in full_config["cameras"].keys():
+        assert os.path.exists(save_loc / f"{camera_name}.mp4")
+        assert os.path.exists(save_loc / f"{camera_name}.metadata.csv")
+
+    # Check that the video has the right number of frames
+    # NB: this won't work unless we mux the videos, so this also tests the muxing.
     for camera_name in full_config["cameras"].keys():
         assert count_frames(str(save_loc / f"{camera_name}.mp4")) == n_test_frames
