4bc96f64f48ef90070b09cda18adebf07a63da1f
jackransomlovell <jlovell@mit.edu>
2024-01-04 12:51:41 -0500

    arduino class now has docstrings and config compatability

--- multicamera_acquisition/interfaces/arduino.py
+++ multicamera_acquisition/interfaces/arduino.py
@@ -76,33 +76,69 @@ def find_serial_ports():
     return result
 
 class Arduino(object):
-    def __init__(self, fps):
-        self.fps = fps
-        return None
-    def generate_basler_frametimes(self, nazures=2, azure_offset=10, basler_offset=10, camera='top'):
 
+    """
+    Represents an Arduino setup for synchronization between cameras.
+
+    Attributes:
+    - config (dict): Configuration dictionary containing parameters like FPS, pulse durations, cycle durations,
+                     camera type, and offsets for synchronization.
+    - fps (int): Frames per second of the system.
+    - azure_pulse_dur (int): Duration of the ir pulse for the Azure camera.
+    - acq_cycle_dur (int): Duration of the acquisition cycle.
+    - camera_type (str): Type of the camera to trigger; either 'top' or 'bottom'.
+    - basler_offset (int): Offset duration for Basler camera pulse.
+    - azure_offset (int): Offset duration for Azure camera pulse.
+    - n_azures (int): Number of Azure cameras
+
+    """
+
+
+    def __init__(self, config):
+        
+        self.config = config
+        self.fps = self.config['fps']
+        self.azure_pulse_dur = self.config['azure_pulse_dur']
+        self.acq_cycle_dur = self.config['acq_cycle_dur']
+        self.camera_type = self.config['camera_type_arduino']
+        self.basler_offset = self.config['basler_offset']
+        self.azure_offset = self.config['azure_offset']
+        self.n_azures = self.config['n_azures']
+
+        return None
+    def generate_basler_frametimes(self):
+        """
+        Generate trigger times for Basler camera frames accounting for Azure camera synchronization.
+
+        Args:
+        - self: The instance of the class.
+
+        Returns:
+        - list: A list of timestamps representing Basler camera frame times adjusted for Azure synchronization.
+
+        Raises:
+        - ValueError: If the provided FPS is not within the valid supported frame rates [30, 60, 90, 120, 150].
+        - ValueError: If the camera parameter is not 'top' or 'bottom'.
+        """
         valid_fps = [30, 60, 90, 120, 150]
         assert self.fps in valid_fps, ValueError(f'fps not in {valid_fps}')
 
-        # hardcoded values
-        pulse_dur = 160
-        cyc_dur = 33333
-        # convert to microseconds 
+        # convert to microseconds
         interframe_interval = (1/self.fps)*1e6
         # get nframes per cycle
-        nframes = np.ceil(cyc_dur / interframe_interval).astype(int)
+        nframes = np.ceil(self.acq_cycle_dur / interframe_interval).astype(int)
 
-        if camera =='top':
+        if self.camera_type =='top':
             bottom_offset = 0
-        elif camera == 'bottom':
+        elif self.camera_type == 'bottom':
             bottom_offset = 1750
         else:
             raise ValueError('camera must be one of the following: top, bottom')
-        
+
         times = []
         # get times
         for n in range(nframes):
-            t = (interframe_interval * n) + (nazures * pulse_dur) + azure_offset + basler_offset
+            t = (interframe_interval * n) + (self.nazures * self.azure_pulse_dur) + self.azure_offset + self.basler_offset
             t += bottom_offset
             times.append(t)
 
@@ -110,7 +146,7 @@ class Arduino(object):
         if self.fps in (120, 150):
             times[1] += 510
         # edge case to deal with last frame interfering with azure
-        if self.fps == 150 and camera =='bottom':
+        if self.fps == 150 and self.camera_type =='bottom':
             times[-1] -= 330
 
         return times
