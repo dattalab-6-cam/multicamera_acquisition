b97856702dc573fc828f1db0e69246a2fe2223c1
JP_6cam_rig <jonahpearl@g.harvard.edu>
2024-01-26 14:59:35 -0500

    downsample display before queueing

--- multicamera_acquisition/acquisition.py
+++ multicamera_acquisition/acquisition.py
@@ -107,9 +107,7 @@ class AcquisitionLoop(mp.Process):
         else:
             self.acq_config = acq_loop_config
 
-        # Check for Nones in camera / writer configs
-        # The idea here is that by now, we should have already
-        # resolved any need to use default configs.
+        # Check for Nones in camera config
         if self.camera_config is None:
             raise ValueError("Camera config cannot be None")
 
@@ -121,6 +119,7 @@ class AcquisitionLoop(mp.Process):
         """Get the default config for the acquisition loop."""
         return {
             "display_every_n": 1,
+            "downsample": 4,
             "dropped_frame_warnings": False,
             "max_frames_to_acqure": None,
         }
@@ -235,7 +234,7 @@ class AcquisitionLoop(mp.Process):
                         if self.camera_config["display"]["display_frames"]:
                             if n_frames_received % self.display_every_n == 0:
                                 self.display_queue.put(
-                                    tuple([depth, camera_timestamp, n_frames_received])
+                                    tuple([depth[::self.acq_config["downsample"], ::self.acq_config["downsample"]], camera_timestamp, n_frames_received])
                                 )
                     else:
                         camera_timestamp = data[1]
@@ -243,6 +242,11 @@ class AcquisitionLoop(mp.Process):
                         self.write_queue.put(data)
                         if self.camera_config["display"]["display_frames"]:
                             if n_frames_received % self.acq_config["display_every_n"] == 0:
+                                data = (
+                                    data[0][::self.acq_config["downsample"], ::self.acq_config["downsample"]],
+                                    data[1],
+                                    data[2]
+                                )
                                 self.display_queue.put(data)
 
                     # Check if we dropped any frames
