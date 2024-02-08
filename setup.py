a15b3d719ab0afd56d24bf0ef7673d5b251f7312
Caleb Weinreb <calebsw@gmail.com>
2024-02-08 12:56:22 -0500

    bugfix

--- setup.py
+++ setup.py
@@ -6,29 +6,6 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="multicamera_acquisition",
-<<<<<<< Updated upstream
-    packages=find_packages(),
-    version="0.1.0",
-    description="Python packaamera acquisition with Flir and Basler cameras.",
-    author="dattalab",
-    license="MIT",
-    install_requires=[
-        "av"
-        "matplotlib"
-        "notebook"
-        "numpy"
-        "opencv-python"
-        "pandas"
-        "pathlib2"
-        "Pillow"
-        "pyserial"
-        "pyusb"
-        "pyyaml"
-        "pypylon"
-        "tqdm"
-    ],
-=======
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
->>>>>>> Stashed changes
 )
