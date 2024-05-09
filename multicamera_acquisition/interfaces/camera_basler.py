d57d31f010060a75ee7643a0c479d78031a9da96
JP_6cam_rig <jonahpearl@g.harvard.edu>
2024-05-09 18:17:08 -0400

    troubleshooting gamma

--- multicamera_acquisition/interfaces/camera_basler.py
+++ multicamera_acquisition/interfaces/camera_basler.py
@@ -278,6 +278,7 @@ class BaslerCamera(BaseCamera):
             pass
 
         # Set gamma
+        # self.logger.info(f'Gamma is {self.config["gamma"]}')
         self.cam.Gamma.SetValue(self.config["gamma"])
 
         # Set exposure time
